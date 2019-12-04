694bde8cac6404836806564f4d82cecf96813abd
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-12-04 10:18:32 +0530

    Commit#21

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
