400fabaf3fa1acc3d5c9a5a34f830da9dbe39c35
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-12-07 22:27:04 +0530

    Commit#37

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -22,7 +22,7 @@ export class HomeComponent implements OnInit,OnDestroy {
   		setInterval( () => {
   			observer.next(count);
   			if(count > 3){
-  				observer.error(new Error('Error has occured more than 3 times.'));
+  				observer.error(new Error('Error has occured more than 4 times.'));
   			}
   			count++;
   		}, 1000);
