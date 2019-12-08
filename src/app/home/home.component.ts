0bcf0fbaf0c09557503574642795948a7a73ea94
AmeyaChavre <ameyachavre.cse.nitrr@gmail.com>
2019-12-08 13:03:32 +0530

    Commit#44

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,10 +13,11 @@ export class HomeComponent implements OnInit,OnDestroy {
   constructor() { }
 
   ngOnInit() {
+  	/*
   	this.firstObsSubscription = interval(1000).subscribe(count => {
   		console.log(count);
   	});
-
+	*/
   	const customIntervalObservable = Observable.create(observer => {
   		let count = 0;
   		setInterval( () => {
